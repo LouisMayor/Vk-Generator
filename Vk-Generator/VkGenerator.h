6af547086d7ce0f54f2227bd5db2762d49d3d344
LouisMayor <louismayor1@gmail.com>
2019-03-01 16:08:31 +0000

    project init

---
+++ Vk-Generator/VkGenerator.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+class VkGenerator
+{
+public:
+	/* constructor/destructor */
+	VkGenerator( ) = default;
+	~VkGenerator( ) = default;
+
+	/* copy */
+	VkGenerator( const VkGenerator& _other ) = delete;
+	void operator=( const VkGenerator& _other ) = delete;
+
+	/* move */
+	VkGenerator( const VkGenerator&& _other ) = delete;
+	void operator=( const VkGenerator&& _other ) = delete;
+private:
+};
